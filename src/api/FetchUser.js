89518cf6ba6c5a953ef43bd59a75358acdca92f8
Annie-92 <ani.keushguerian@gmail.com>
2024-11-05 18:58:48 +0400

    “updates“

--- src/api/FetchUser.js
+++ src/api/FetchUser.js
@@ -5,28 +5,35 @@ import { API_URL } from '../constants/apiConstants';
 
 const getToken = () => localStorage.getItem('token');
 
-console.log('localStorage');
-console.log(localStorage);
 
 const fetchUserData = async () => {
   try {
     const token = getToken();
     const response = await axios.get(API_URL + "UserData.php", {
       headers: {
-        token: `${token}`
-      }
+       token: `${token}`
+       }
     });
+
+
     console.log('user Data Log'); // Add this line to see what data is being returned
     console.log(response.data); // Add this line to see what data is being returned
 
     response.data.is_teacher = parseInt(response.data.is_teacher, 10);
 
+    console.log('response');
+    console.log(response);
+
     
    return response.data;
   } catch (error) {
-    console.error('Error fetching user data:', error);
-    // Handle error (redirect to login page, display error message, etc.)
-    return null; // or throw error, depending on your use case
+    if (error.response) {
+      console.error('Response data:', error.response.data);
+    } else if (error.request) {
+      console.error('Request made but no response received:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
   }
 };
 
--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,7 @@
-// export const API_URL = 'http://localhost:8888/abac/webAPI/v1/';
-export const API_URL = 'https://abac.colitely.com/webAPI/v1/';
+export const API_URL = 'http://localhost:8888/abac/webAPI/v1/';
+// export const API_URL = 'https://abac.colitely.com/webAPI/v1/';
 
-//  export const PUBLIC_URL = 'http://localhost:8888/abac';
-export const PUBLIC_URL = 'https://abac.colitely.com';
+// export const API_URL = '/webAPI/v1/';
+
+ export const PUBLIC_URL = 'http://localhost:8888/abac';
+// export const PUBLIC_URL = 'https://abac.colitely.com';
